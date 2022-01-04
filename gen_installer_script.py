155869bb942f43c9835b579ffaf9afe885cf8bdb
SodaCris <18463922396@163.com>
2022-01-04 11:27:38 +0800

    format

--- gen_installer_script.py
+++ gen_installer_script.py
@@ -1,15 +1,14 @@
 from get_version import get_version
 
-if __name__=='__main__':
+if __name__ == "__main__":
     lines = []
-    with open("installer.pre.nsi", mode='r', encoding='utf-8-sig') as f:
+    with open("installer.pre.nsi", mode="r", encoding="utf-8-sig") as f:
         lines = f.readlines()
     version = get_version()
 
     for i in range(len(lines)):
-        lines[i] = lines[i].replace('{version}', version)
-    
-    with open("installer.out.nsi", mode='w', encoding='utf-8-sig') as f:
+        lines[i] = lines[i].replace("{version}", version)
+
+    with open("installer.out.nsi", mode="w", encoding="utf-8-sig") as f:
         for line in lines:
             f.write(line)
-
