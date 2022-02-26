8b4e0c9e6d0effdddd30203d72590b43273bffad
SodaCris <18463922396@163.com>
2022-02-26 21:06:37 +0800

    修复cmake编译错误

--- build.ps1
+++ build.ps1
@@ -1,12 +1,16 @@
 if (Get-Variable 'foo' -Scope 'Global' -ErrorAction 'Ignore') { 
-} else {
-    ms22
-    if(Test-Path build){
+}
+else {
+    ms22_32
+    if (Test-Path build) {
         Remove-Item -Path build -Recurse -Force
     }
     cmake -B build -G "Ninja" `
-    -DCMAKE_TOOLCHAIN_FILE="D:/src/vcpkg/scripts/buildsystems/vcpkg.cmake"
-    $global:foo = 1
+        -DCMAKE_TOOLCHAIN_FILE="D:/src/vcpkg/scripts/buildsystems/vcpkg.cmake"
+
+    if ($LASTEXITCODE = =0) {
+        $global:foo = 1
+    }
 }
 
 cmake --build build
