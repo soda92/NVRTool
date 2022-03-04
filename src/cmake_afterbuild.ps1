9eeadd11ef0373abde74e72f93ef17c51a3dad4c
SodaCris <18463922396@163.com>
2022-03-04 23:47:57 +0800

    添加Python依赖库

--- src/cmake_afterbuild.ps1
+++ src/cmake_afterbuild.ps1
@@ -12,9 +12,10 @@ Copy-Item -Path ./*.exe $dest
 Copy-Item -Path ./*.dll $dest
 Copy-Item -Path $PSScriptRoot/../CHANGELOG.md $dest
 
-Copy-Item -Path D:/HCNetSDK/lib/* -Destination $dest -Recurse -Force
+Copy-Item -Path D:/HCNetSDK/lib/*.dll -Destination $dest -Recurse -Force
+Copy-Item -Path D:/HCNetSDK/lib/HCNetSDKCom -Destination $dest -Recurse -Force
 
 Copy-Item -Path C:/WINDOWS/SYSTEM32/mfc140d.dll -Destination $dest
 
-Copy-Item -Path D:/src/py-TaiYuan/src/*.py -Destination $dest
-Copy-Item -Path D:/src/py-TaiYuan/src/*.ini -Destination $dest
+Copy-Item -Path $PSScriptRoot/../pysrc/*.py -Destination $dest
+Copy-Item -Path $PSScriptRoot/../pysrc/*.ini -Destination $dest
