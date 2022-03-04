6ab2d831ef8773b09ee6a83a5e99ffdfcf3788e3
SodaCris <18463922396@163.com>
2022-03-04 22:50:11 +0800

    复制库

--- src/cmake_afterbuild.ps1
+++ src/cmake_afterbuild.ps1
@@ -13,3 +13,8 @@ Copy-Item -Path ./*.dll $dest
 Copy-Item -Path $PSScriptRoot/../CHANGELOG.md $dest
 
 Copy-Item -Path D:/HCNetSDK/lib/* -Destination $dest -Recurse -Force
+
+Copy-Item -Path C:/WINDOWS/SYSTEM32/mfc140d.dll -Destination $dest
+
+Copy-Item -Path D:/src/py-TaiYuan/src/*.py -Destination $dest
+Copy-Item -Path D:/src/py-TaiYuan/src/*.ini -Destination $dest
