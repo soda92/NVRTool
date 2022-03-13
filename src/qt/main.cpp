af168a372e4e6e7d881b81b699ac8d47b8970095
SodaCris <18463922396@163.com>
2022-03-14 02:06:51 +0800

    添加测试内存

--- src/qt/main.cpp
+++ src/qt/main.cpp
@@ -2,6 +2,33 @@
 #include <QtWidgets>
 #include <QMainWindow>
 #include <QDebug>
+#include <memory>
+#include <QFile>
+#include <QTextStream>
+#include <fmt/core.h>
+#include <QPointer>
+using namespace std;
+
+class A
+{
+public:
+    inline static int count = 0;
+    A()
+    {
+        count += 1;
+        fmt::print("object count: {}\n", count);
+    };
+    void print()
+    {
+        fmt::print("A used\n");
+    };
+    ~A()
+    {
+        count -= 1;
+        fmt::print("object count: {}\n", count);
+        fmt::print("A deleted\n");
+    };
+};
 
 class MainWindow : public QMainWindow
 {
@@ -10,35 +37,65 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QMainWindow *parent = 0) : QMainWindow(parent)
     {
-        button_ = new QPushButton(tr("Push Me!"));
-        textBrowser_ = new QTextBrowser();
+        auto topbar_w = new QWidget;
+        auto topbar = new QHBoxLayout;
+        topbar_w->setLayout(topbar);
+        auto label = new QLabel(tr("Keyword:"));
+        topbar->addWidget(label);
+        topbar->addWidget(lineedit);
 
-        QGridLayout *mainLayout = new QGridLayout;
-        mainLayout->addWidget(button_, 0, 0);
-        mainLayout->addWidget(textBrowser_, 1, 0);
-        QWidget *widget = new QWidget();
-        widget->setLayout(mainLayout);
-        setCentralWidget(widget);
-        widget->setMinimumSize(800, 600);
+        topbar->addWidget(btn);
+        auto full = new QWidget;
+        auto l2 = new QVBoxLayout;
+        full->setLayout(l2);
+        l2->addWidget(topbar_w);
+        l2->addWidget(textedit);
+        a = new A;
+        // a = new A;
+        // a = new A;
+
+        setCentralWidget(full);
+        full->setMinimumSize(800, 600);
         setWindowTitle(tr("Connecting buttons to processes.."));
+        connect(btn, &QPushButton::clicked, this, &MainWindow::on_findButton_clicked);
 
-        connect(button_, &QPushButton::clicked, this, &MainWindow::clicked);
+        loadTextFile();
     };
     ~MainWindow()
     {
-        delete button_;
-        delete textBrowser_;
+        delete a;
+    }
+public slots:
+    void on_findButton_clicked()
+    {
+        QString searchString = lineedit->text();
+        fmt::print("1\n");
+        {
+            auto a = make_unique<A>();
+            a.get()->print();
+        }
+        fmt::print("2\n");
+        textedit->find(searchString, QTextDocument::FindWholeWords);
     };
 
-public slots:
-    void clicked()
-    {
-        qDebug() << "clicked";
-    }
-
 private:
-    QPushButton *button_;
-    QTextBrowser *textBrowser_;
+    QPushButton *btn = new QPushButton(tr("Find"));
+    QTextEdit *textedit = new QTextEdit();
+    QLineEdit *lineedit = new QLineEdit();
+    A *a;
+    void loadTextFile()
+    {
+        QFile inputFile(":/input.txt");
+        inputFile.open(QIODevice::ReadOnly);
+
+        QTextStream in(&inputFile);
+        QString line = in.readAll();
+        inputFile.close();
+
+        textedit->setPlainText(line);
+        QTextCursor cursor = textedit->textCursor();
+        cursor.movePosition(QTextCursor::Start, QTextCursor::MoveAnchor, 1);
+    };
 };
 
 #include "main.moc"
